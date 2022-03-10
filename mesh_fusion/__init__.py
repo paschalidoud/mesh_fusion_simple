566ab8e146a6f015e728c97aaab35f3e689e0d9d
Despoina Paschalidou <paschalidoud@gmail.com>
2022-03-10 14:18:15 -0800

    Add setup.py

---
+++ mesh_fusion/__init__.py
@@ -0,0 +1,8 @@
+"""Convert meshes to watertight meshes using TSDF fusion."""
+
+__author__ = "Despoina Paschalidou"
+__license__ = "MIT"
+__maintainer__ = "Despoina Paschalidou"
+__email__ = "paschald@stanford.edu"
+__url__ = "https://paschalidoud.github.io/"
+__version__ = "0.1"
