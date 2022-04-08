e1543f572ee74eb51d36f8864f0af3da99aa82aa
Despoina Paschalidou <paschalidoud@gmail.com>
2022-04-08 13:54:49 -0700

    Enable passing a single mesh as input

--- scripts/make_mesh_watertight.py
+++ scripts/make_mesh_watertight.py
@@ -23,8 +23,8 @@ def main(argv):
         description="Convert non-watertight meshes to watertight"
     )
     parser.add_argument(
-        "path_to_mesh",
-        help="Path to the mesh to be converted"
+        "path_to_meshes",
+        help="Path to folder containing the mesh/meshes to be converted"
     )
     parser.add_argument(
         "path_to_output_directory",
@@ -39,33 +39,44 @@ def main(argv):
     # Disable trimesh's logger
     logging.getLogger("trimesh").setLevel(logging.ERROR)
 
-    path_to_file = os.path.join(
-        args.path_to_output_directory, "model_watertight.obj"
-    )
+    if os.path.isdir(args.path_to_meshes):
+        path_to_meshes = [
+            os.path.join(args.path_to_meshes, mi)
+            for mi in os.listdir(args.path_to_meshes)
+            if mi.endswith(".obj") or mi.endswith(".off")
+        ]
+    else:
+        path_to_meshes = [args.path_to_meshes]
+
     # Check optimistically if the file already exists
-    ensure_parent_directory_exists(path_to_file)
+    ensure_parent_directory_exists(args.path_to_output_directory)
 
-    # Load and scale the mesh to range [-0.5,0.5]^3
-    raw_mesh = Mesh.from_file(args.path_to_mesh)
-    raw_mesh.to_unit_cube()
-    # Extract the points and the faces from the raw_mesh
-    points, faces = raw_mesh.to_points_and_faces()
+    for pi in path_to_meshes:
+        file_name = pi.split("/")[-1].split(".")[0]
+        path_to_output_file = os.path.join(
+            args.path_to_output_directory, f"{file_name}.off"
+        )
+        # Load and scale the mesh to range [-0.5,0.5]^3
+        raw_mesh = Mesh.from_file(pi)
+        raw_mesh.to_unit_cube()
+        # Extract the points and the faces from the raw_mesh
+        points, faces = raw_mesh.to_points_and_faces()
 
-    tr_mesh = trimesh.Trimesh(vertices=points, faces=faces)
-    # Make the mesh watertight with TSDF Fusion
-    tsdf_fuser = TSDFFusion()
-    tr_mesh_watertight = tsdf_fuser.to_watertight(
-        tr_mesh, path_to_file
-    )
-    # Call the meshlabserver to simplify the mesh
-    simplification_script = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        args.path_to_simplification_script
-    )
-    subprocess.call([
-        "meshlabserver"
-        f" -i {path_to_file} -o {path_to_file} -s {simplification_script}",
-    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+        tr_mesh = trimesh.Trimesh(vertices=points, faces=faces)
+        # Make the mesh watertight with TSDF Fusion
+        tsdf_fuser = TSDFFusion()
+        tr_mesh_watertight = tsdf_fuser.to_watertight(
+            tr_mesh, path_to_output_file
+        )
+        # Call the meshlabserver to simplify the mesh
+        simplification_script = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            args.path_to_simplification_script
+        )
+        subprocess.call([
+            "meshlabserver"
+            f" -i {path_to_output_file} -o {path_to_output_file} -s {simplification_script}",
+        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
 
 
 if __name__ == "__main__":
